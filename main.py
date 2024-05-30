2a1184832a76232af0aa6443004414098b529105
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2024-05-30 23:15:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -187,22 +187,8 @@ def change_prompt(update: Update, context: CallbackContext) -> None:
     new_promt = " ".join(context.args)
     logger.info(f"chatId({chat_id}) changed its Promt to :'{new_promt}'")
     if new_promt :
-        if new_promt.lower != 'd':
-    
-            try:
-                model_temp_ = genai.GenerativeModel(
-                    model_name="gemini-1.5-pro-latest",
-                    safety_settings=safety_settings,
-                    generation_config=generation_config,
-                    system_instruction=new_promt )
-                chat_histories[chat_id] = model_temp_.start_chat(history=[])
-    
-                update.message.reply_text(f"The prompt has been successfully changed to: <b>'{new_promt}'</b>", parse_mode='html')
-            except Exception as e:
-                update.message.reply_text(f"The prompt has failed to changed <b>error: '{e}' </b>", parse_mode='html')
-    
-        elif new_promt.lower == 'd':
-                 model_temp = genai.GenerativeModel(
+        if  context.args[0].strip().lower() == 'd':
+           model_temp = genai.GenerativeModel(
                     model_name="gemini-1.5-pro-latest",
                     safety_settings=safety_settings,
                     generation_config=generation_config,
@@ -211,7 +197,17 @@ def change_prompt(update: Update, context: CallbackContext) -> None:
     
                  chat_histories[chat_id] = model_temp_.start_chat(history=[])
                  update.message.reply_text(f"The prompt has been successfully changed to: <b>'defult'</b>", parse_mode='html')
-
+    
+            
+        else:
+                model_temp_ = genai.GenerativeModel(
+                    model_name="gemini-1.5-pro-latest",
+                    safety_settings=safety_settings,
+                    generation_config=generation_config,
+                    system_instruction=new_promt )
+                chat_histories[chat_id] = model_temp_.start_chat(history=[])
+    
+                update.message.reply_text(f"The prompt has been successfully changed to: <b>'{new_promt}'</b>", parse_mode='html')
     else:
             update.message.reply_text(f"Error ! un sufficent info provided", parse_mode='html')
 
